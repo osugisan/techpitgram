261bcabe8e02df500aadc3f3ac86e3b892d33094
Ubuntu <ubuntu@ip-172-31-75-162.ec2.internal>
2020-11-10 10:01:37 +0000

    start

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    controllers: { registrations: 'registrations' }
+    
   root 'pages#home'
+  get '/users/:id', to: 'users#show', as: 'user'
+  resources :posts, only: [:new, :create] do
+    resources :photos, only: [:create]
+  end
+  
 end
